ddf4e0a32e8fb91aae5f49915059c8dd5f477b3e
Yudelka <yudelkaguillen07@gmail.com>
2024-02-12 18:51:26 -0400

    Cambio de nombre a DAL y validaciones

---
+++ DAL/Contexto.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Parcial1_AP1_YudelkaGuillen.Models;
+
+namespace Parcial1_AP1_YudelkaGuillen.Context
+{
+    public class Contexto: DbContext
+    {
+        public DbSet<Metas> metas { get; set; }
+        public Contexto(DbContextOptions<Contexto> Options):base (Options) { }
+    }
+}
--- Models/Metas.cs
+++ Models/Metas.cs
@@ -1,10 +1,20 @@
-﻿namespace Parcial1_AP1_YudelkaGuillen.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Parcial1_AP1_YudelkaGuillen.Models
 {
     public class Metas
     {
+        [Key]
+
         public int MetasId { get; set; }
+
+        [Required(ErrorMessage ="Es requerido")]
+        [DataType(DataType.Date)]
+        [Range(typeof(DateTime), "1900-01-01", "2100-12-31")]
         public DateTime? Fecha { get; set; }
+        [Required(ErrorMessage = "Es requerido")]
         public string? Descripcion { get; set; }
+        [Required(ErrorMessage = "Es requerido")]
         public double Monto { get; set; }
     }
 }
